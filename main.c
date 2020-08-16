c74bb6385f676c8e313072841f9b0131da8b24f9
dodo1254 <do_125@hotmail.co.th>
2020-08-16 14:31:05 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) 
+{
+int a,b;
+float ans;
+printf("Enter A : ");
+scanf("%d", &a);
+printf("Enter B : ");
+scanf("%d", &b);
+ans = 0.5 * a * b;
+printf("Answer is %f" ,ans); 
+return 0;
+}
